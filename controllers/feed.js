3cb4f5381a73dec20552e9434da791556d3d5898
Muskan3dev <muskannovember3@gmail.com>
2024-04-10 20:37:40 +0530

    Added getPost middleware for fetching single post and modified getPosts middleware for fetching all posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,9 +3,18 @@ const { check, validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post" }],
-  });
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "posts fetched successfully", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -32,6 +41,27 @@ exports.createPost = (req, res, next) => {
         message: "Post created successfully",
         post: result,
       });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  console.log({ postId: postId });
+  Post.findById(postId)
+    .exec()
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
     })
     .catch((err) => {
       if (!err.statusCode) {
