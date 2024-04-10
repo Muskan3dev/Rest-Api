7926b4a469157ab6023e38471097ff361a92cfbe
Muskan3dev <muskannovember3@gmail.com>
2024-04-10 16:16:09 +0530

    Imported validationResult subpackage from express validator and added error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,7 @@
+const { check, validationResult } = require("express-validator");
+
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{ title: "First Post", content: "This is the first post" }],
@@ -5,11 +9,34 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed,entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  //create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+  const creator = req.body.creator;
+
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duck.jpg",
+    creator: creator,
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
