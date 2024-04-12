fa6a0e892042a4e7afaf2f7f3933de3efac0b2f5
Muskan3dev <muskannovember3@gmail.com>
2024-04-13 00:05:49 +0530

    Fix status path error

--- models/user.js
+++ models/user.js
@@ -18,10 +18,6 @@ const userSchema = new Schema({
     type: String,
     default: "I am new!",
   },
-  status: {
-    type: String,
-    required: true,
-  },
   posts: [
     {
       type: Schema.Types.ObjectId,
